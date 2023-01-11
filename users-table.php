6b8bad5fb823f59d60545210341ebe420b8b2e71
Ahamed Arshad <achchu.zats@gmail.com>
2023-01-12 00:30:21 +0530

    Initial Commit

---
+++ users-table.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name:       Inpsyde Users Table
+ * Plugin URI:        https://github.com/achchu93/inpsyde-users-table
+ * Description:       A custom Users table implementaion in a custom page.
+ * Version:           1.0.0
+ * Author:            Ahamed Arshad
+ * Author URI:        https://github.com/achchu93
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       users-table
+ * Domain Path:       /languages
+ *
+ * @link              https://github.com/achchu93/inpsyde-users-table
+ * @since             1.0.0
+ * @package           Inpsyde\UsersTable
+ */
+
+namespace Inpsyde\UsersTable;
